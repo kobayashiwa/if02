8f22c81319281d849bc12d2647c8d1c9bc4abf8d
kobamov <koba@mov.am>
2020-11-06 14:11:31 +0900

    “Lottieの表示の記述見直し”

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('Lottie'), 
-    renderer: 'svg', // 描画形式
-    loop: true, // trueにしたらループ。1回再生の場合はfalse
-    autoplay: true, // 自動再生
-    path: '../img/34837-devices-switching.json' // jsonのパスを指定
-    name: "Hello World", // 今後のリファレンスとして名前を指定（オプション）
-    });
